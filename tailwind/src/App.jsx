736cd46f511ea304da4528e93fda5b2d2c57f97c
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2025-07-10 15:20:30 +0600

    Tailwind start

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './components/Home'
+
+
+function App() {
+  return (
+    <div>
+      <Home/>
+      <h1 className='text-orange-400 text-center'>Hello dear</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1 className='text-orange-400'>Hello dear friend</h1>
+    </div>
+  )
+}
+
+export default Home
