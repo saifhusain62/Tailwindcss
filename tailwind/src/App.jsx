a1fd325db72bd58dadbec9aeaeee2fe3125c95e8
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2025-07-18 23:55:37 +0600

    background image

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home'
 
 
 
+
 function App() {
   return (
     <div>
@@ -229,6 +230,19 @@ function App() {
                     
 
               </div>
+
+              {/* BACKGROUND IMAGE  */}
+
+
+              <div className='bgarea w-6xl h-96 mx-auto bg-local'>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                       
+              </div>
+               <div >
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur pariatur velit dolorum tenetur dolor non iure, rerum cupiditate eaque eum corrupti inventore praesentium sit labore dignissimos nemo omnis quisquam eius quas distinctio itaque eos fugit! Eum, fuga recusandae asperiores provident quo architecto voluptates ipsum harum. Sit eius expedita cum. Tenetur unde aut modi nemo possimus id, magni accusantium, eaque placeat ut ullam! Iure perferendis temporibus eligendi sequi, magni alias nisi nesciunt quis neque delectus voluptate exercitationem ut molestiae repellat unde pariatur modi possimus sed, explicabo maiores, amet similique odio perspiciatis. Rem sit officia cumque error, inventore nobis facilis hic nemo!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nulla eius incidunt ea sunt voluptates corrupti aut. Aspernatur fugiat nemo aliquam mollitia nulla maiores praesentium aut obcaecati, molestiae rerum assumenda exercitationem est eaque corrupti corporis iure quo debitis quasi delectus. Fugit totam nemo esse temporibus iusto pariatur, autem blanditiis asperiores natus velit accusamus officiis beatae facilis nostrum molestiae assumenda excepturi sit quia dolores perspiciatis sed accusantium officia architecto perferendis! Ex qui similique laborum quo ducimus itaque aliquid molestiae explicabo eius esse tempora repudiandae enim amet est libero sit quaerat iusto deserunt optio quasi perspiciatis, recusandae ipsum iure. Temporibus atque necessitatibus voluptates qui aut minima sint corporis, eos omnis repellat? Qui architecto consequatur facere sit quaerat pariatur odio voluptatem modi excepturi enim hic incidunt ad porro cum voluptatibus eos, ipsa in corrupti labore ipsum unde ducimus? Minus incidunt aliquid pariatur labore repellendus perferendis consequuntur magni totam, voluptatum, maiores suscipit molestias perspiciatis ipsum, sit atque? Quaerat itaque similique officiis dolores veniam tenetur sequi at amet molestias incidunt doloremque odit debitis quas fuga dolor voluptate non architecto aperiam quidem iusto, cum dicta. Assumenda soluta quo atque aut velit reprehenderit perspiciatis vitae placeat nostrum quod maiores, in, quos voluptatum non dicta, dolorum commodi sit?</p>
+                        </div>
+              
 
 
     </div>
